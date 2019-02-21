78653f93b802bb47eb5b925f33c9f4df18a87864
Robert <robertibengle@gmail.com>
2019-02-21 11:24:02 -0800

    Updated opcontrol program for basic functionality 	modified:   README.md 	modified:   compile_commands.json 	modified:   src/opcontrol.cpp

--- .vscode/cquery_cached_index/c@@users@don@documents@robot programs@pros@menutester/src@opcontrol.cpp
+++ .vscode/cquery_cached_index/c@@users@don@documents@robot programs@pros@menutester/src@opcontrol.cpp
@@ -4,6 +4,9 @@
 #define DRIVE_LEFT_BACK_PORT 2
 #define DRIVE_RIGHT_FRONT_PORT 4
 #define DRIVE_RIGHT_BACK_PORT 1
+#define FLYWHEEL_LOWER_PORT 19
+#define FLYWHEEL_UPPER_PORT 11
+#define INTAKE_PORT 3
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -19,17 +22,57 @@
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	//Set up controller
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	//Set up motors
 	pros::Motor driveLFMotor (DRIVE_LEFT_FRONT_PORT);
 	pros::Motor driveLBMotor (DRIVE_LEFT_BACK_PORT);
 	pros::Motor driveRFMotor (DRIVE_RIGHT_FRONT_PORT, true);
 	pros::Motor driveRBMotor (DRIVE_RIGHT_BACK_PORT, true);
+	pros::Motor fwLowerMotor (FLYWHEEL_LOWER_PORT);
+	pros::Motor fwUpperMotor (FLYWHEEL_UPPER_PORT);
+	pros::Motor intakeMotor (INTAKE_PORT);
+	//Initialize shortcut variables
+	short leftJoy_x;
+	short leftJoy_y;
+	short rightJoy_x;
+	short rightJoy_y;
+	bool leftTriggerUp;
+	bool leftTriggerDown;
+	bool rightTriggerUp;
+	bool rightTriggerDown;
+	bool btnA;
+	bool btnB;
+	bool btnX;
+	bool btnY;
+	bool btnUp;
+	bool btnDown;
+	bool btnLeft;
+	bool btnRight;
 	while (true) {
-		//Confiugre drivetrain
-		driveLFMotor.move(master.get_analog(ANALOG_LEFT_Y));
-		driveLBMotor.move(master.get_analog(ANALOG_LEFT_Y));
-		driveRFMotor.move(master.get_analog(ANALOG_RIGHT_Y));
-		driveRBMotor.move(master.get_analog(ANALOG_RIGHT_Y));
+		//Set variables
+		leftJoy_x = (short)master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+		leftJoy_y = (short)master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		rightJoy_x = (short)master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+		rightJoy_y = (short)master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+		leftTriggerUp = master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+		leftTriggerDown = master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
+		rightTriggerUp = master.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
+		rightTriggerDown = master.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
+		btnA = master.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+		btnB = master.get_digital(pros::E_CONTROLLER_DIGITAL_B);
+		btnX = master.get_digital(pros::E_CONTROLLER_DIGITAL_X);
+		btnY = master.get_digital(pros::E_CONTROLLER_DIGITAL_Y);
+		btnUp = master.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
+		btnDown = master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN);
+		btnLeft = master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT);
+		btnRight = master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT);
+		//Drivetrain
+		driveLFMotor.move(leftJoy_y);
+		driveLBMotor.move(leftJoy_y);
+		driveRFMotor.move(rightJoy_y);
+		driveRBMotor.move(rightJoy_y);
+		//Flywheel
 
 		//Dont hog
 		pros::delay(2);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# MenuTester" 
+A MenuTester program for the new VEX V5 Brain. This will mostly be used for testing
+the LittleVGL menu. 
